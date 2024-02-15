b0e756d145dd04cd53ddc9813748a99cb07e7c1f
maxjdev <maxjramosdev@gmail.com>
2024-02-15 09:57:24 -0300

    add: repository layer

---
+++ src/main/java/dio/web/api/repository/UserRepository.java
@@ -0,0 +1,37 @@
+package dio.web.api.repository;
+
+import dio.web.api.model.User;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class UserRepository {
+    public void save(User user) {
+        if(user.getId() == null)
+            System.out.println("***SAVE -> RECEIVING USER IN THE REPOSITORY LAYER | RECEBENDO USUARIO NA CAMADA DE REPOSITÓRIO***");
+        else
+            System.out.println("***UPDATE -> RECEIVING USER IN THE REPOSITORY LAYER | RECEBENDO USUARIO NA CAMADA DE REPOSITÓRIO***");
+        System.out.println(user);
+    }
+    public void deleteById(Integer id) {
+        System.out.println(String.format("DELETE/id - RECEIVING ID: %d TO DELETE A USER | RECEBENDO O ID: %d PARA EXCLUIR UM USUARIO", id));
+        System.out.println(id);
+    }
+    public List<User> findAll() {
+        System.out.println("LIST - LISTING SYSTEM USERS | LISTANDO OS USUARIOS DO SISTEMA");
+        List<User> users = new ArrayList<>();
+        users.add(new User("1UserName1", "1MyP@55w0rD!1"));
+        users.add(new User("2UserName2", "2MyP@55w0rD!2"));
+        return users;
+    }
+    public User findById(Integer id) {
+        System.out.println(String.format("FIND/id - RECEIVING ID: %d TO FIND A USER | RECEBENDO O ID: %d PARA LOCALIZAR UM USUARIO", id));
+        return new User("1UserName1", "1MyP@55w0rD!1");
+    }
+    public User findByUsername(String username) {
+        System.out.println(String.format("FIND/username - RECEIVING USER: %s TO FIND A USER | RECEBENDO O USUARIO: %s PARA LOCALIZAR UM USUARIO", username));
+        return new User("1UserName1", "1MyP@55w0rD!1");
+    }
+}
